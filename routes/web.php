6f1456be981d0a10142b017eac786aa1c654c916
Dr. Peter Schulze-Buxloh <peter@schu-bu.de>
2021-05-15 10:53:31 +0200

    added ui auth

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('/blog/{id}', function ($id) {
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
