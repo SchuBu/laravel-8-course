e506e43f8f3ab5bca021d8b6ed9d7a2f03c38a69
Dr. Peter Schulze-Buxloh <peter@schu-bu.de>
2021-12-26 12:15:48 +0100

    upgraded tailwindcss to v.3.0.x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,13 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    mode: 'jit',
-    purge: [
+    content: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
         './vendor/usernotnull/tall-toasts/config/**/*.php',
         './vendor/usernotnull/tall-toasts/resources/views/**/*.blade.php',
     ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                lime: colors.lime,
-            }
-        },
-    },
-    variants: {
-        extend: {},
-    },
     plugins: [
         require('@tailwindcss/line-clamp'),
     ],
